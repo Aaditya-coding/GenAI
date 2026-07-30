01b989000c15d2e6b09154fe097d9bb99f479fa4
Aaditya-coding <aaditya200614@gmail.com>
2026-07-30 17:08:20 +0530

    30th July, 2026

---
+++ 3. EmbeddedModels/4_document_similarity.py
@@ -0,0 +1,25 @@
+from langchain_ollama import OllamaEmbeddings
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+embedding = OllamaEmbeddings(model="nomic-embed-text",dimensions=300)
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famously known for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = 'tell me about bumrah'
+
+document_embeddings = embedding.embed_documents(documents)
+query_embeddings = embedding.embed_query(query)
+
+scores = cosine_similarity([query_embeddings], document_embeddings)[0]
+
+index, score = sorted(list(enumerate(scores)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score is: ",score)
